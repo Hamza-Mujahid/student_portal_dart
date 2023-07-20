bc7f50e4a39ff068733c079dd25f0f800fcb0f7f
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2023-07-20 09:16:14 +0500

    updated

--- studentCrudOps.dart
+++ studentCrudOps.dart
@@ -1,37 +1,42 @@
-import './db.dart';
+// student cruds
 
-createStudent(
-    {required String name,
-    required String email,
-    required String password,
-    required String course}) {
-  Map newstudent = {
+List createStudent({
+  required String name,
+  required String email,
+  required String password,
+  required String course,
+  required List studentsData,
+}) {
+  Map<String, dynamic> newStudent = {
     "id": studentsData.length,
     "name": name,
     "email": email,
     "password": password,
-    "course": course
+    "course": course,
   };
-  studentsData.add(newstudent);
+  studentsData.add(newStudent);
+  print(studentsData);
   return studentsData;
 }
 
-Map<dynamic, dynamic>? getStudentById(int id) {
+Map<dynamic, dynamic>? getStudentById(int id, List studentsData) {
   for (var student in studentsData) {
     if (student["roll"] == id) {
+      print(student["name"]);
       return student;
     }
   }
   return null;
 }
 
-void updateStudent(int id, Map<String, dynamic> updatedData) {
-  Map<dynamic, dynamic>? student = getStudentById(id);
+void updateStudent(
+    int id, Map<String, dynamic> updatedData, List studentsData) {
+  Map<dynamic, dynamic>? student = getStudentById(id, studentsData);
   if (student != null) {
     student.addAll(updatedData);
   }
 }
 
-void deleteStudent(int id) {
+void deleteStudent(int id, List studentsData) {
   studentsData.removeWhere((student) => student["roll"] == id);
 }
--- teacher.dart
+++ teacher.dart
@@ -1,56 +1,74 @@
-import 'dart:io';
-import 'index.dart';
-import './db.dart';
-import 'studentCrudOps.dart';
+// import 'dart:io';
+// import 'index.dart';
+// import './db.dart';
+// import 'studentCrudOps.dart';
 
-Map teacher = {};
+// Map teacher = {};
 
-teacherLogin() {
-  print('welcome Teacher');
-  print('Enter email and password');
+// teacherLogin() {
+//   print('welcome Teacher');
+//   print('Enter email and password');
 
-  bool loginSuccessful = false;
+//   bool loginSuccessful = false;
 
-  while (!loginSuccessful) {
-    String tEmail = stdin.readLineSync()!;
-    String tPassword = stdin.readLineSync()!;
-    for (Map th in teachersData) {
-      if (tEmail == th["email"] && tPassword == th["password"]) {
-        loginSuccessful = true;
-        teacher = th;
-      }
-    }
+//   while (!loginSuccessful) {
+//     String tEmail = stdin.readLineSync()!;
+//     String tPassword = stdin.readLineSync()!;
+//     for (Map th in teachersData) {
+//       if (tEmail == th["email"] && tPassword == th["password"]) {
+//         loginSuccessful = true;
+//         teacher = th;
+//       }
+//     }
 
-    if (loginSuccessful) {
-      print('Login successful!');
+//     if (loginSuccessful) {
+//       print('Login successful!');
 
-      teacherFunction();
-    } else {
-      print('Login failed. Incorrect email or password.');
-      print('Please enter your email and password again:');
-    }
-  }
-}
+//       teacherFunction();
+//     } else {
+//       print('Login failed. Incorrect email or password.');
+//       print('Please enter your email and password again:');
+//     }
+//   }
+// }
 
-teacherFunction() {
-  bool teacherWorking = true;
-  while (teacherWorking) {
-    print('enter add to add a new student');
-    print('enter get to get a student data using his id');
-    print("enter update to update a student's data");
-    print('enter add to add a new student');
-    String ipnut = stdin.readLineSync()!;
-    if (ipnut == 'add') {
-      print("enter Student Name");
-      String name = stdin.readLineSync()!;
-      print("enter Student email");
-      String email = stdin.readLineSync()!;
-      print("set Student Password");
-      String password = stdin.readLineSync()!;
-      print("What course is Student gonna be joining?");
-      String course = stdin.readLineSync()!;
-      createStudent(
-          name: name, email: email, password: password, course: course);
-    }
-  }
-}
+// teacherFunction() {
+//   bool teacherWorking = true;
+//   while (teacherWorking) {
+//     print('Enter "add" to add a new student');
+//     print('Enter "get" to get a student data using their id');
+//     print('Enter "update" to update a student\'s data');
+//     print('Enter "exit" to stop');
+//     String input = stdin.readLineSync()!;
+//     if (input == 'add') {
+//       print("Enter Student Name:");
+//       String name = stdin.readLineSync()!;
+//       print("Enter Student Email:");
+//       String email = stdin.readLineSync()!;
+//       print("Set Student Password:");
+//       String password = stdin.readLineSync()!;
+//       print("What course is the student going to join?");
+//       String course = stdin.readLineSync()!;
+//       createStudent(
+//         name: name,
+//         email: email,
+//         password: password,
+//         course: course,
+//         studentsData: studentsData,
+//       );
+//       getStudentById(studentsData.length, studentsData)
+//       print("Student added successfully!");
+//     } else if (input == 'get') {
+//       // function 
+//     } else if (input == 'update') {
+//       // Implement code to update a student's data (not shown in the provided code).
+//       // You can prompt for the student's id, select the student from studentsData,
+//       // and then update the relevant fields.
+//     } else if (input == 'exit') {
+//       teacherWorking = false;
+//       print("Exiting teacher function.");
+//     } else {
+//       print("Invalid input. Try again.");
+//     }
+//   }
+// }
